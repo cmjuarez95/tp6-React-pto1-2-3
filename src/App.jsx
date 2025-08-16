8eb592ab77fe9a8388a5e6e933c9a22b0e1a7130
Mariano Juarez <cm.juarez.95@gmail.com>
2025-08-16 12:43:19 -0300

    punto 3 realizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
+
 function App() {
   const prop = "my friend";
 
+  // definimos el state
+  const [msj, setMsj] = useState(""); 
+
+  // función que cambia el state al hacer click
+  const handleClick = () => {
+    setMsj("(from changed state)");
+  }
+
   return (
+
     
-    <main className="container-fluid d-flex">
-      <h1>Hello {prop}!</h1>
+    
+    <main className="container-fluid d-flex flex-column">
+      <h1>Hello {prop} {msj}! </h1>
+      <button className="btn btn-primary w-25" onClick={handleClick}>Mostrar mensaje</button>
+      {/* mostramos el state cuando se actualice */}
+      
     </main>
      
   )
